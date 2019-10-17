b7dbbab08d619154230946618c7a5cf7a1af85a6
RJ <therjmurray@gmail.com>
2019-10-17 15:37:46 -0400

    trying different things

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
-const { constants, hiscores } = require("osrs-api");
+const { hiscores } = require("osrs-json-api");
 const express = require('express')
 const app = express()
 const axios = require('axios').default;
+var path = require('path');
 
 const port = 3000
 
-var charStats = hiscores.getPlayer({ name: "jZERKk", type: constants.playerTypes.normal }).then(console.log())
+// const charStats = hiscores.getPlayer({ name: "jZERKk", type: constants.playerTypes.normal })
+// .then(function(res){
+//   var g = (res.overall.rank)
+//   console.log(g)
+// })
 
-app.get('/', (req, res) => res.send(charStats))
+hiscores
+.getPlayer('jZERKk')
+.then((res) => res.json())
+.then((data) => {
+  
+})
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname + '/index.html'))
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
