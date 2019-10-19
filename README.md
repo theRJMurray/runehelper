651e3453b9f25e25a986c6807bb33ad0d5ef48ed
turtleman270 <ahill5067@gmail.com>
2019-10-19 09:26:54 -0400

    display highscores and add tests

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
-#RuneHelper
+# RuneHelper
 Helping people efficiently build their OSRS character
 
-How to run this application:
+## Running the app
+Clone this repository `git clone git@github.com:theRJMurray/runehelper.git`
+`npm install`
+`npm start`
+In your browser go to http://localhost:3000/<username> where <username> is the
+username of the character you want to lookup
+
+## Running tests
+`npm install`
+`npm test`
---
+++ tests/index.js
@@ -0,0 +1,42 @@
+const chai = require('chai')
+const chaiHttp = require('chai-http')
+const app = require('../index')
+
+chai.use(chaiHttp);
+chai.should();
+describe("Highscores", () => {
+  describe("GET/", () => {
+
+    it("should get highscore data for jZERKk", (done) => {
+      chai.request(app)
+        .get('/jZERKk')
+        .end((err, res) => {
+          res.should.have.status(200)
+          res.body.should.be.a('object')
+          done()
+        })
+    })
+
+    it("should fail when not provided a username", (done) => {
+      chai.request(app)
+        .get('/')
+        .end((err, res) => {
+          res.should.have.status(404)
+          done()
+        })
+    })
+
+    it("should fail when username is invalid", (done) => {
+      chai.request(app)
+        .get('/superLongUsernameThatRunescapeWouldntAllow')
+        .end((err, res) => {
+          res.should.have.status(200)
+          res.body.should.equal("An error occurred")
+          done()
+        })
+    })
+
+
+
+  })
+})
