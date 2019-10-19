56dbeb11fbfedc9c8b2bb3dca4a8cd783595bcf6
RJ <therjmurray@gmail.com>
2019-10-19 12:57:26 -0400

    cleaned up sendFile in root directory functionality

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port = 3000
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname + '/public/index.html'))
+  res.sendFile(path.join(__dirname + 'index.html'))
 });
 
 app.get('/:username', function(req, res, error){
